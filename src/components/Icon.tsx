e4915a04a307aa491700a14dcae8ca87aea3d6aa
chyerin802 <chyerin802@gmail.com>
2024-08-31 16:27:31 +0900

    feat: Icon component

---
+++ src/components/Icon.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+// import SVG for React Component
+import CancelIcon from '@assets/icons/cancel.svg?react';
+import VisibilityIcon from '@assets/icons/visibility.svg?react';
+
+// Icon name
+type IconName = 'cancel' | 'visibility';
+interface IconProps extends React.SVGProps<SVGSVGElement> {
+	name: IconName;
+}
+
+const Icon: React.FC<IconProps> = ({ name, ...props }) => {
+	const icons: Record<IconName, JSX.Element> = {
+		// 사용할 아이콘을 추가
+		cancel: <CancelIcon {...props} />,
+		visibility: <VisibilityIcon {...props} />,
+	};
+
+	return icons[name];
+};
+
+export default Icon;
