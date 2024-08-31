d8792f4fc59897455046dc99b3b06f9e1b181f16
chyerin802 <chyerin802@gmail.com>
2024-08-31 16:56:16 +0900

    feat: visibility-off icon

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -2,9 +2,10 @@ import React from 'react';
 // import SVG for React Component
 import CancelIcon from '@assets/icons/cancel.svg?react';
 import VisibilityIcon from '@assets/icons/visibility.svg?react';
+import VisibilityOffIcon from '@assets/icons/visibility_off.svg?react';
 
 // Icon name
-type IconName = 'cancel' | 'visibility';
+type IconName = 'cancel' | 'visibility' | 'visibilityOff';
 interface IconProps extends React.SVGProps<SVGSVGElement> {
 	name: IconName;
 }
@@ -14,6 +15,7 @@ const Icon: React.FC<IconProps> = ({ name, ...props }) => {
 		// 사용할 아이콘을 추가
 		cancel: <CancelIcon {...props} />,
 		visibility: <VisibilityIcon {...props} />,
+		visibilityOff: <VisibilityOffIcon {...props} />,
 	};
 
 	return icons[name];
