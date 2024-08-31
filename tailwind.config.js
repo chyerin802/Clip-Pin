9fafa53c027bb8a775587591880e918c07e8b6a2
chyerin802 <chyerin802@gmail.com>
2024-08-31 18:17:51 +0900

    feat : tailwind color theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 import formPlugin from '@tailwindcss/forms';
+const colors = require('tailwindcss/colors');
 
 export default {
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
-		extend: {},
+		colors: {
+			white: colors.white,
+			brand: {
+				primary: colors.violet[500],
+				secondary: colors.violet[300],
+			},
+			point: {
+				primary: colors.emerald[500],
+				secondary: colors.emerald[200],
+			},
+			neutral: {
+				primary: colors.neutral[900],
+				secondary: colors.neutral[500],
+				tertiary: colors.neutral[300],
+			},
+			error: {
+				primary: colors.red[500],
+				secondary: colors.red[300],
+			},
+		},
 	},
 	plugins: [formPlugin],
 };
